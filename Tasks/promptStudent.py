9baca2f3579489444b658309f3c8e3f3413d906f
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2024-07-24 10:37:22 +0100

    Updated promptStudent()
    Updated promptStudent()

--- Tasks/promptStudent.py
+++ Tasks/promptStudent.py
@@ -1,8 +1,8 @@
-def promptStudent(student_number):
+def promptStudent():
     """
     Prompts a User to enter details and maps to the student_detail dictionary
 
-    Parameters:
+    Parameters:s
             student_name : Name of the student.
             student_number : The student's unique identifier.
             course_name : The student's course name.
